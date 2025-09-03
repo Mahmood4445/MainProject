df9a612925be3fb6a3c75f7ddb2e6b10ccee5784
Mahmood4445 <mt6622814@gmail.com>
2025-09-03 15:04:08 +0530

    production change 3

--- Frontend/tailwind.config.ts
+++ Frontend/tailwind.config.ts
@@ -5,7 +5,9 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
+  prefix: "",
   theme: {
     container: {
       center: true,
