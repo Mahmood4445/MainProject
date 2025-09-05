0fdb1b6d363e4a6dfd914bab58d724926ffc892a
Mahmood4445 <mt6622814@gmail.com>
2025-09-05 11:42:35 +0530

    create table for payment process

--- Backend/api/admin.py
+++ Backend/api/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
 from django.contrib import admin
-from .models import Review
+from .models import Review, Payment
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ("author_name", "rating", "created_at")
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("reference_number", "status", "amount", "currency", "created_at", "paid_at")
